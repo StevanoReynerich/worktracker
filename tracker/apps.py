e03accc4f8c4a8acc681681c8ed0b660772c1e03
Stevano Reynerich <stevanoreynerich@RichMacbooks.local>
2026-05-18 11:00:57 +0800

    auto create admin user

--- tracker/apps.py
+++ tracker/apps.py
@@ -2,5 +2,27 @@ from django.apps import AppConfig
 
 
 class TrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tracker'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tracker"
+
+    def ready(self):
+        import os
+        from django.contrib.auth.models import User
+        from django.db.utils import OperationalError, ProgrammingError
+
+        username = os.environ.get("ADMIN_USERNAME")
+        email = os.environ.get("ADMIN_EMAIL")
+        password = os.environ.get("ADMIN_PASSWORD")
+
+        if not username or not password:
+            return
+
+        try:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email or "",
+                    password=password
+                )
+        except (OperationalError, ProgrammingError):
+            pass
