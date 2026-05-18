1f197cf7a14623b99f6fd74c8092471d63661aa7
Stevano Reynerich <stevanoreynerich@RichMacbooks.local>
2026-05-18 10:29:28 +0800

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ua-@a!#qh+k^1wnt(jk$2_nmu&qm#bqab#a*^w#f=gpvaslfv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "worktracker-lbur.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
